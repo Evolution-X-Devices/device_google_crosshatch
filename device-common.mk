64219941fde9c14de95951cc7bf9552dbca184c2
TechPanelGM <giomook2003@gmail.com>
2026-03-26 13:13:23 +0000

    b1c1: Ship gcam

--- device-common.mk
+++ device-common.mk
@@ -23,6 +23,9 @@ PRODUCT_PLATFORM := sdm845
 
 include device/google/crosshatch/device.mk
 
+# Ship GCam
+$(call inherit-product, vendor/Camera/b1c1/config.mk)
+
 # Target now uses mini_gapps
 TARGET_USES_MINI_GAPPS := true
 
